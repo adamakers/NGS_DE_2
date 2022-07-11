b69e2749752c72e51765ae6687396810798f420d
Adam Akers <adamrakers@gmail.com>
2022-07-10 21:55:05 -0600

    moved the map element into its own class

--- src/app.js
+++ src/app.js
@@ -1,45 +1,80 @@
+class Map {
+  constructor(mapEl) {
+    this.markersArr = [];
+    this.mapEl = mapEl;
+    this.mainMap = L.map(mapEl, {
+      zoomControl: false
+    }).setView([38.952, -104.789], 15);
 
-const map = L.map('map', {
-  zoomControl: false
-}).setView([38.952, -104.789], 15);
-
-L.control.zoom({
-  position: 'bottomright'
-}).addTo(map);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  minZoom: 5,
-  attribution: '© OpenStreetMap'
+    L.control.zoom({
+      position: 'bottomright'
+    }).addTo(this.mainMap);
     
-}).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      minZoom: 5,
+      attribution: '© OpenStreetMap'
+    }).addTo(this.mainMap);
+  }
+
+  // add a point
+  createPoint(lat, lon) {
+    this.markersArr.push(L.marker([lat, lon]));
+  }
+
+  drawMarkers() {
+    this.removeAllMarkers();
+
+    // adding all markers
+    this.markersArr.forEach( point => {
+      point.addTo(this.mainMap);
+    });
+  }
+
+  removeAllMarkers() {
+    this.markersArr.forEach( point => {
+      this.mainMap.removeLayer(point);
+    });
+  }
+}
 
 
-class DataGrabber = { 
+class Monument {
+  constructor(lat, lon, type, text) {
+    this.lat = lat;
+    this.lon = lon;
+    this.type = type;
+    this.text = text;
+  }
+}
+
+class SearchAside {
+  // create tile El from point
+
+  // create put all Els in aside
+
+  //
+}
+
+class DataGrabber {
+  // gets the api data
 
 }
 
 
+const mainMap = new Map('map');
+
+mainMap.createPoint(38.952, -104.789);
+mainMap.createPoint(38.962, -104.789);
+mainMap.createPoint(38.972, -104.789);
+mainMap.createPoint(38.982, -104.789);
+mainMap.drawMarkers();
 
 
 
 
 
-
-
-
-
-let marker = L.marker([38.952, -104.789]);
-
-marker.addTo(map);
-
-
-
-
-
-
-
-
+document.querySelector('.btn').addEventListener('click', mainMap.removeAllMarkers.bind(mainMap));
 
 
 
